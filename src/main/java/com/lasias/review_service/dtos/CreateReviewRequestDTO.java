5265838b78a6e5692b9a2960ef27fde5e841dc98
ivbe0111 <berezkinivan94@gmail.com>
2026-08-18 15:35:07 +0200

    added CreateReviewRequestDTO

---
+++ src/main/java/com/lasias/review_service/dtos/CreateReviewRequestDTO.java
@@ -0,0 +1,30 @@
+package com.lasias.review_service.dtos;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+
+@Getter
+public class CreateReviewRequestDTO {
+
+    @Size(max = 1000)
+    private final String comment;
+
+    @Min(1)
+    @Max(5)
+    private final int rating;
+
+    @NotNull
+    private final Long bookingId;
+    @NotNull
+    private final Long roomTypeId;
+
+    public CreateReviewRequestDTO(String comment, int rating, Long bookingId, Long roomTypeId) {
+        this.comment = comment;
+        this.rating = rating;
+        this.bookingId = bookingId;
+        this.roomTypeId = roomTypeId;
+    }
+}
