bc854a03dc7df6c2e59e6cbbf9f03a29e1d8985c
ivbe0111 <berezkinivan94@gmail.com>
2026-08-18 16:44:09 +0200

    started working on GetControllers

---
+++ src/main/java/com/lasias/review_service/controllers/GetControllers.java
@@ -0,0 +1,20 @@
+package com.lasias.review_service.controllers;
+
+import com.lasias.review_service.dtos.ShowReviewResponseDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/review")
+public class GetControllers {
+
+    //Get all users reviews
+   // @GetMapping("/my")
+   // public ResponseEntity<List<ShowReviewResponseDTO>> getMyReviews(@AuthenticationPrincipal )
+}
